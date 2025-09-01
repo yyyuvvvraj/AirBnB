b7bf2edc5c9b961da255c24c7a38b827b99b43c9
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2025-09-01 18:48:48 +0530

    Phase 1(b)

--- api/app.js
+++ api/app.js
@@ -1,104 +1,83 @@
+// /api/app.js
+
 const express = require("express");
+const mongoose = require("mongoose");
+const Listing = require("../models/listing");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
 const app = express();
-const mongoose=require("mongoose");
-const Listing= require("../models/listing");
-const path=require("path");
-const { title } = require("process");
-const methodOverride=require("method-override");
-const ejsMate=require("ejs-mate");
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
-
-main().then(()=>{
+// Connect to MongoDB Atlas using environment variable
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     console.log("MongoDB connected");
-})
-.catch((err)=>{
-    console.log(err);
-})
-async function main(){
-    await mongoose.connect(MONGO_URL);
-}
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"/views"));
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.engine("ejs", ejsMate);
+// Serve static files (such as styles and scripts)
+app.use(express.static(path.join(__dirname, "../public")));
 
-
-app.get("/",(req,res)=>{
-    res.send("Hi,I am root");
-});
-//Index Route
-app.get("/listings",async (req,res)=>{
-    const allListings = await Listing.find({});
-    res.render("./listings/index.ejs",{allListings});
+// Root route
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
 });
 
-//New Route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
+// Listings index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
 
-//Show Route
-app.get("/listings/:id",async (req,res)=>{
-    let {id}=req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
+// New listing form route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
 });
 
-function normalizeImageUrl(listingData) {
-  if (listingData.image && listingData.image.url === "") {
-    listingData.image.url = undefined;
-  }
-  return listingData;
-}
-
-
-//Create Route
-app.post("/listings",async (req,res)=>{
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
+// Show listing details
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
 
-//Edit Route
-app.get("/listings/:id/edit",async (req,res)=>{
-    let { id }=req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{ listing });
+// Create listing route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
 
-//Update Route
-app.put("/listings/:id",async (req,res)=>{
-    let { id }=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
+// Edit listing route
+app.get("/listings/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
 });
 
-//Delete Route
-app.delete("/listings/:id",async (req,res)=>{
-    let { id }=req.params;
-    let deleted = await Listing.findByIdAndDelete(id);
-    console.log(deleted);
-    res.redirect("/listings");
+// Update listing route
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
 });
 
-
-// app.get("/testlisting",async (req,res)=>{
-//     let sampleListing=new Listing({
-//         title:"My New Villa",
-//         description:"A beautiful villa in the heart of the city",
-//         price:12000,
-//         location:"Almatty",
-//         country:"Kazakhstan"
-//     });
-//     await sampleListing.save();
-//     console.log("Listing saved");
-//     res.send("Successfully added a new listing");
-// });
-
-app.listen(8080,()=>{
-    console.log("Server is running on port 8080");
+// Delete listing route
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const deleted = await Listing.findByIdAndDelete(id);
+  console.log(deleted);
+  res.redirect("/listings");
 });
+
+// Export for Vercel serverless function
+module.exports = app;
