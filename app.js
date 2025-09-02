47b921933b1b7c9d931e99b7dba723cc96e2c9c7
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2025-09-02 21:08:09 +0530

    Phase 1(b)

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
-const mongoose=require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('DB connected'))
+.catch(err => console.error('DB connection error:', err));
 const Listing= require("./models/listing");
 const path=require("path");
 const { title } = require("process");
